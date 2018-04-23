ea39e4762eed5e444b51dcd18e3c140a14bbbe85
ssharmapraveen2007 <ssharmapraveen2007@gmail.com>
2018-04-24 01:49:57 +0530

    In gallery page more images added

--- src/gallery/gallery.js
+++ src/gallery/gallery.js
@@ -45,7 +45,160 @@ const imgUrl13 = require('../assets/images/VisitingCard_Back.jpg');
 const imgUrl14 = require('../assets/images/Invite.jpg');
 const imgUrl15 = require('../assets/images/Inspirations.jpg');
 
+const imgUrl34 = require('../assets/images/Goa.jpg');
+const imgUrl16 = require('../assets/images/Manali.jpg');
+const imgUrl17 = require('../assets/images/RoomsOne.jpg');
+const imgUrl18 = require('../assets/images/new-brochure.jpg');
+const imgUrl19 = require('../assets/images/broucher_aspen.jpg');
+const imgUrl20 = require('../assets/images/broucher_aspen_2.jpg');
+const imgUrl21 = require('../assets/images/broucher_aspen_2_modified.jpg');
+const imgUrl22 = require('../assets/images/broucher_aspen_3.jpg');
+const imgUrl23 = require('../assets/images/broucher_aspen_modified.jpg');
+const imgUrl24 = require('../assets/images/cd-cover.jpg');
+const imgUrl25 = require('../assets/images/CD-cover2.jpg');
+const imgUrl26 = require('../assets/images/CD-cover_new.jpg');
+const imgUrl27 = require('../assets/images/CD-cover_new2.jpg');
+const imgUrl28 = require('../assets/images/CD-cover_new3.jpg');
+const imgUrl29 = require('../assets/images/CD-cover_new4.jpg');
+const imgUrl30 = require('../assets/images/CD-cover_new5.jpg');
+const imgUrl31 = require('../assets/images/CD-cover_new6.jpg');
+const imgUrl32 = require('../assets/images/CD-cover_new7.jpg');
+const imgUrl33 = require('../assets/images/CD-cover_new8.jpg');
+
 const IMAGES = [
+	{
+		src: imgUrl34,
+		thumbnail: imgUrl34,
+		thumbnailWidth: 250,
+		thumbnailHeight: 174,
+		caption: 'Goa Tours Web page Home'
+	},
+	{
+		src: imgUrl16,
+		thumbnail: imgUrl16,
+		thumbnailWidth: 250,
+		thumbnailHeight: 174,
+		caption: 'Manali Tours Web page Home'
+	},
+	{
+		src: imgUrl17,
+		thumbnail: imgUrl17,
+		thumbnailWidth: 200,
+		thumbnailHeight: 174,
+		caption: 'RoomsOne Web page Home'
+	},
+	{
+		src: imgUrl18,
+		thumbnail: imgUrl18,
+		thumbnailWidth: 250,
+		thumbnailHeight: 174,
+		caption: 'New brochure design'
+	},
+	{
+		src: imgUrl19,
+		thumbnail: imgUrl19,
+		thumbnailWidth: 250,
+		thumbnailHeight: 174,
+		caption: 'broucher aspen'
+	},
+	{
+		src: imgUrl20,
+		thumbnail: imgUrl20,
+		thumbnailWidth: 250,
+		thumbnailHeight: 174,
+		caption: 'broucher_aspen_2'
+	},
+	{
+		src: imgUrl21,
+		thumbnail: imgUrl21,
+		thumbnailWidth: 250,
+		thumbnailHeight: 174,
+		caption: 'broucher_aspen_2_modified'
+	},
+	{
+		src: imgUrl22,
+		thumbnail: imgUrl22,
+		thumbnailWidth: 250,
+		thumbnailHeight: 174,
+		caption: 'broucher_aspen_3'
+	},
+	{
+		src: imgUrl23,
+		thumbnail: imgUrl23,
+		thumbnailWidth: 250,
+		thumbnailHeight: 174,
+		caption: 'broucher_aspen_modified'
+	},
+	{
+		src: imgUrl24,
+		thumbnail: imgUrl24,
+		thumbnailWidth: 300,
+		thumbnailHeight: 174,
+		caption: 'cd-cover'
+	},
+	{
+		src: imgUrl25,
+		thumbnail: imgUrl25,
+		thumbnailWidth: 250,
+		thumbnailHeight: 174,
+		caption: 'CD-cover2'
+	},
+	{
+		src: imgUrl26,
+		thumbnail: imgUrl26,
+		thumbnailWidth: 250,
+		thumbnailHeight: 174,
+		caption: 'CD-cover_new'
+	},
+	{
+		src: imgUrl27,
+		thumbnail: imgUrl27,
+		thumbnailWidth: 250,
+		thumbnailHeight: 174,
+		caption: 'CD-cover_new2'
+	},
+	{
+		src: imgUrl28,
+		thumbnail: imgUrl28,
+		thumbnailWidth: 250,
+		thumbnailHeight: 174,
+		caption: 'CD-cover_new3'
+	},
+	{
+		src: imgUrl29,
+		thumbnail: imgUrl29,
+		thumbnailWidth: 250,
+		thumbnailHeight: 174,
+		caption: 'CD-cover_new4'
+	},
+	{
+		src: imgUrl20,
+		thumbnail: imgUrl30,
+		thumbnailWidth: 250,
+		thumbnailHeight: 174,
+		caption: 'CD-cover_new5'
+	},
+	{
+		src: imgUrl31,
+		thumbnail: imgUrl31,
+		thumbnailWidth: 250,
+		thumbnailHeight: 174,
+		caption: 'CD-cover_new6'
+	},
+	{
+		src: imgUrl32,
+		thumbnail: imgUrl32,
+		thumbnailWidth: 250,
+		thumbnailHeight: 174,
+		caption: 'CD-cover_new7'
+	},
+	{
+		src: imgUrl33,
+		thumbnail: imgUrl33,
+		thumbnailWidth: 250,
+		thumbnailHeight: 174,
+		caption: 'CD-cover_new8'
+	},
 	{
 		src: imgUrl15,
 		thumbnail: imgUrl15,
